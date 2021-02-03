85e4967798887b207f28ebc5403ee6f4a0b3dbd1
Fblach <fblach32@gmail.com>
2021-02-03 17:44:50 -0300

    ItemCounter

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import gif from './oprah.gif'
+import ItemCounter from '../ItemCounter/ItemCounter'
 
 function ItemListContainer(){
+
+    let stock = 10;
+
+    const handleAdd = (counter) => {
+        return () => {
+            if (counter > 0) {
+                alert(counter + ' items added')
+            }
+            else {
+                alert('No items to add')
+            }
+        }
+    }
     return(
         <div style={{display: 'flex', justifyContent: 'center'}}>
-         <img src={gif} alt="ERROR" ></img>
+            <img src={gif} alt="ERROR" ></img>
+            <ItemCounter onAdd={handleAdd} stock={stock} inicial={1} />
         </div>
     )
 }
